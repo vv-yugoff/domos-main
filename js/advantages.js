2fb3b570a30c654725329e51b3a03e4a72ef9716
Yugoff <vv-yugov@yandex.ru>
2023-09-07 00:16:00 +0500

    There is a filter left, index.html and intro

--- js/advantages.js
+++ js/advantages.js
@@ -1,15 +1,32 @@
-const text = document.querySelector(".text-advantages p");
-const letters = text.innerText.split("");
+// const text = document.querySelector(".text-advantages p");
+// const letters = text.innerText.split("");
 
-const spannedText = letters.map((letter, i) => {
+// const spannedText = letters.map((letter, i) => {
+//     const span = document.createElement("span");
+    
+//     span.innerText = letter;
+//     span.style.position = "absolute";
+//     span.style.transform = `rotate(${i * 10}deg)`;
+    
+//     text.appendChild(span);
+// });
+
+// text.querySelector('.text-advantages-title').style.color = "transparent";
+
+const texts = document.querySelectorAll(".text-advantages p");
+
+texts.forEach(text => {
+  const letters = text.innerText.split("");
+
+  const spannedText = letters.map((letter, i) => {
     const span = document.createElement("span");
+    
     span.innerText = letter;
     span.style.position = "absolute";
-    return span;
-});
-
-text.innerHTML = "";
-
-spannedText.forEach(span => {
+    span.style.transform = `rotate(${i * 10}deg)`;
+    
     text.appendChild(span);
+  });
+
+  text.querySelector('.text-advantages-title').style.color = "transparent";
 });
