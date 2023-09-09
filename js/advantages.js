a47e5a8527b191470b09326a6c4e08caae9000b0
vachagunn <vachikpuper@gmail.com>
2023-09-09 15:34:45 +0500

    added the ability to expand text in a article.

--- js/advantages.js
+++ js/advantages.js
@@ -1,18 +1,3 @@
-// const text = document.querySelector(".text-advantages p");
-// const letters = text.innerText.split("");
-
-// const spannedText = letters.map((letter, i) => {
-//     const span = document.createElement("span");
-    
-//     span.innerText = letter;
-//     span.style.position = "absolute";
-//     span.style.transform = `rotate(${i * 10}deg)`;
-    
-//     text.appendChild(span);
-// });
-
-// text.querySelector('.text-advantages-title').style.color = "transparent";
-
 const texts = document.querySelectorAll(".text-advantages p");
 
 texts.forEach(text => {
