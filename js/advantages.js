95c9d3dce1f4a1c07413ef26938becc4c2400531
Yugoff <vv-yugov@yandex.ru>
2023-09-13 00:50:52 +0500

    fix top-bar

--- js/advantages.js
+++ js/advantages.js
@@ -1,17 +1,70 @@
-const texts = document.querySelectorAll(".text-advantages p");
+// const texts = document.querySelectorAll(".text-advantages p");
 
-texts.forEach(text => {
-  const letters = text.innerText.split("");
+// texts.forEach(text => {
+//   const letters = text.innerText.split("");
 
-  const spannedText = letters.map((letter, i) => {
-    const span = document.createElement("span");
+//   const spannedText = letters.map((letter, i) => {
+//     const span = document.createElement("span");
     
-    span.innerText = letter;
-    span.style.position = "absolute";
-    span.style.transform = `rotate(${i * 10}deg)`;
+//     span.innerText = letter;
+//     span.style.position = "absolute";
+//     span.style.transform = `rotate(${i * 10}deg)`;
     
-    text.appendChild(span);
-  });
+//     text.appendChild(span);
+//   });
 
-  text.querySelector('.text-advantages-title').style.color = "transparent";
-});
+//   text.querySelector('.text-advantages-title').style.color = "transparent";
+// });
+
+	/*-----------------------------------------------------*/
+  	/* Navigation Menu
+   ------------------------------------------------------ */
+   var toggleButton = $('.menu-toggle'),
+       nav = $('.main-navigation');
+
+   // toggle button
+   toggleButton.on('click', function(e) {
+
+		e.preventDefault();
+		toggleButton.toggleClass('is-clicked');
+		nav.slideToggle();
+
+	});
+
+   // nav items
+  	nav.find('li a').on("click", function() {
+
+   	// update the toggle button
+   	toggleButton.toggleClass('is-clicked');
+   	// fadeout the navigation panel
+   	nav.fadeOut();
+
+  	});
+
+
+   /*---------------------------------------------------- */
+  	/* Highlight the current section in the navigation bar
+  	------------------------------------------------------ */
+	var sections = $("section"),
+	navigation_links = $("#main-nav-wrap li a");
+
+	sections.waypoint( {
+
+       handler: function(direction) {
+
+		   var active_section;
+
+			active_section = $('section#' + this.element.id);
+
+			if (direction === "up") active_section = active_section.prev();
+
+			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+         navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+
+		},
+
+		offset: '25%'
+	});
+
