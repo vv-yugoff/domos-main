6f2bf567d03335e903413473a12da164a3fe3c78
Yugoff <vv-yugov@yandex.ru>
2023-09-18 14:34:14 +0500

    push vachik-new

---
+++ js/data/realtors-data.js
@@ -0,0 +1,22 @@
+export const realtors = [
+    {
+        name: 'Светлана',
+        image: 'svetlana',
+        type: 'Квартиры',
+        country: 'Россия',
+        region: 'Свердловская область',
+        city: 'Екатеринбург',
+        experience: 'До 5 лет',
+        sex: 'Женский'
+    },
+    {
+        name: 'Георгий',
+        image: 'georgiy',
+        type: 'Дома',
+        country: 'Россия',
+        region: 'Челябинская область',
+        city: 'Челябинск',
+        experience: 'От 5 лет',
+        sex: 'Мужской'
+    },
+];
