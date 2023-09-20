4e3fcf0cd5c88040444387bd953d821439182259
vachagunn <vachikpuper@gmail.com>
2023-09-20 20:37:34 +0500

    Смена текста по клику на картинку в блоке "Кейсы"

---
+++ js/data/cases-data.js
@@ -0,0 +1,17 @@
+export const cases = [
+    {
+        id: 1,
+        name: 'name_1',
+        text: 'text_1'
+    },
+    {
+        id: 2,
+        name: 'name_2',
+        text: 'text_2'
+    },
+    {
+        id: 3,
+        name: 'name_3',
+        text: 'text_3'
+    }
+];
--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,38 @@
+import { cases } from "./data/cases-data.js";
+
 // Смена изображений в слайдере
 
 const mainImage = document.querySelector('#main-image');
 const previews = document.querySelectorAll('.slider-preview-item a');
+const name = document.querySelector('#case-realtor-name');
+const text = document.querySelector('#case-text');
+
+renderBlock();
 
 for (let activeImage of previews) {
     activeImage.onclick = function(evt) {
         evt.preventDefault();
-        mainImage.src = activeImage.href;
-
         let currentActive = document.querySelector('.slider-preview-item .active');
+        
+        mainImage.src = activeImage.href;
+        
         currentActive.classList.remove('active');
         activeImage.classList.add('active');
+        
+        changeText(activeImage);
     }
+}
+
+function renderBlock() {
+    name.innerHTML = cases[0].name;
+    text.innerHTML = cases[0].text;
+}
+
+function changeText(active) {
+    cases.forEach((item) => {
+        if (Number(active.id) === item.id) {
+            name.innerHTML = item.name;
+            text.innerHTML = item.text;
+        }
+    });
 }
