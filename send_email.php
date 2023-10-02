fac16246d63f85ee1d1eaa9d023124c4e342b9d5
vachagunn <vachikpuper@gmail.com>
2023-10-02 21:19:21 +0500

    Карточки реалторов + почта

--- send_email.php
+++ send_email.php
@@ -2,23 +2,22 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $phone = $_POST['phone'];
-    $email = $_POST['email'];
+    // $email = $_POST['email'];
 
     // Формирование заголовков письма
-    $headers = "From: $name <$email>\n";
-    $headers .= "Reply-To: $name <$email>\n";
+    // $headers = "From: $name <$email>\n";
+    // $headers = "Reply-To: $name <$email>\n";
 
     // Текст письма
     $message = "
         Вы получили новое сообщение от $name\n
         Номер телефона: $phone\n
-        Почта: $email
     ";
 
     // Отправка письма
     $to = 'vachikpuper@gmail.com';
     $subject = 'Новая заявка с сайта';
-    mail($to, $subject, $message, $headers);
+    mail($to, $subject, $message);
 
     //Отправка успешного ответа на клиентскую сторону
     echo json_encode(['success' => true]);
